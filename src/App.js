8ea5a206334f9b8dd6ea211a374bffd7bb208199
19097 <jayneaniello27@gmail.com>
2020-03-30 10:59:19 -0500

    Petfinder API Start

--- src/App.js
+++ src/App.js
@@ -8,8 +8,49 @@ import { NavigationBar } from './Components/Header';
 import { NoMatch } from './NoMatch';
 import { Search } from './Search'; 
 import { Layout } from './Components/Layout';
+import pf from "petfinder-client";
+import Pet from "./Pet"
+
+const petfinder = pf({
+  key: process.env.yR0OMcncKcTmfuAuyREFp6tZowt6gDsynub72rliOeMnpuNCrj,
+  secret: process.env.zkHIDM4bKmBSyEhI6bZlmATxWGAQJ0dmxHw2PU5j
+});
+
+
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+      
+        this.state = {
+          pets: []
+        };
+      }
+      // replace cDM
+      componentDidMount() {
+        petfinder.pet
+          .find({ location: "Austin, TX", output: "full" })
+          .then(data => {
+            let pets;
+            if (data.petfinder.pets && data.petfinder.pets.pet) {
+              if (Array.isArray(data.petfinder.pets.pet)) {
+                pets = data.petfinder.pets.pet;
+              } else {
+                pets = [data.petfinder.pets.pet];
+              }
+            } else {
+              pets = []
+            }
+            this.setState({
+              pets
+            });
+          });
+      }
+    // inside class, above render
+componentDidMount() {
+    petfinder.breed.list({ animal: "dog" }).then(console.log, console.error);
+  }
     render() {
         return (
             <>
@@ -24,6 +65,28 @@ class App extends Component {
                 </Switch>
                 </Router>
                 </Layout>
+
+                <h1>Adopt Me!</h1>
+                <pre>
+  <code>{JSON.stringify(this.state, null, 2)}</code>
+</pre>
+      {this.state.pets.map(pet => {
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        return (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            name={pet.name}
+            breed={breed}
+          />
+        );
+      })}
+   
             </>
         );
     }
