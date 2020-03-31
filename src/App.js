b8bb7c96912a6c3b0ae7e5a7a61b3ee8ea8e64b1
19097 <jayneaniello27@gmail.com>
2020-03-31 09:38:28 -0500

    updated working search bar

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ const App = () => {
 
   return (
   <div className="App">
-     <form className="search-form">
-      <input className="search-bar" type="text"/>
-      <button className="search-button" type="submit"> 
-      Search
-      </button>
-    </form>
+    <Search />
     <Random />
   </div>
   )
